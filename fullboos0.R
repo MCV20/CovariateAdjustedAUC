325a9ac7e75e0917776e15de358466d86ad8486a
MCV20 <144927857+MCV20@users.noreply.github.com>
2026-01-11 21:39:11 -0400

    Add files via upload

---
+++ fullboos0.R
@@ -0,0 +1,274 @@
+library(haven)
+library(dplyr)
+library(nnet)
+
+
+df <- read_sas("/home/Projects/LungCancerAUC/cmnanal.sas7bdat", NULL)
+
+df <- df %>% 
+  transmute(
+    A   = as.numeric(rndgroup == 1),
+    Y   =ifelse(!is.na(cancyr) & cancyr <= 6, 1L, 0L),
+    age = age,
+    race      = case_when(
+      ethnic == 1 ~ "Hispanic",
+      race == 1 ~ "White",
+      race == 2 ~ "Black",
+      race == 3 ~ "Asian",
+      race == 4 ~ "American Indian or Alaskan Native",
+      race == 5  ~ "Native Hawaiian or Pacific Islander",
+      .default = NA_character_
+    ),
+    education      = recode(
+      as.character(educat),
+      `1` = 1,
+      `2` = 1,
+      `3` = 2,
+      `4` = 3,
+      `5` = 4,
+      `6` = 5,
+      `7` = 6,
+      .default = NA_real_
+    ),
+    BMI = 703 * weight / height^2,
+    COPD = diagcopd,
+    PHC = ifelse(cancblad == 1 | cancbrea == 1 | canccerv == 1 | canccolo == 1 |
+                   cancesop == 1 | canckidn == 1 | canclung == 1 | cancnasa == 1 |
+                   cancoral == 1 | cancpanc == 1 | cancphar == 1 | cancstom == 1 |
+                   cancthyr == 1 | canctran == 1, 1, 0),
+    FHC = ifelse(fambrother == 1 | famchild == 1 | famfather == 1 | fammother == 1 | famsister == 1, 1, 0),
+    packyears = pkyr,
+    cigsmok   = cigsmok,
+    smokeday = smokeday,
+    smokeyr = smokeyr,
+    smoke_inconsistent = (cigsmok == 1 & !is.na(age_quit)),
+    age_quit_clean = if_else(cigsmok == 1, NA_real_, age_quit),
+    quit_time = case_when(
+      cigsmok == 0 & !is.na(age_quit_clean) ~ pmax(0, age - age_quit_clean),
+      cigsmok == 1 ~ 0,         
+      TRUE ~ NA_real_             
+    )
+  ) %>%
+  select(!age_quit_clean)
+
+df <- df[complete.cases(df),]
+
+
+## Take Coefficients from model (PAPER)
+beta <- c(`(Intercept)` = -4.532506, 
+          age = 0.0778868,
+          raceBlack = 0.3944778,
+          raceHispanic = -0.7434744, 
+          raceAsian = -0.466585,
+          `raceAmerican Indian or Alaskan Native` = 0,
+          `raceNative Hawaiian or Pacific Islander`= 1.027152,
+          education = -0.0812744,
+          BMI =  -0.0274194,
+          COPD = 0.3553063,
+          PHC = 0.4589971,
+          FHC = 0.587185,
+          cigsmok = 0.2597431,
+          smokeday = -1.822606,
+          smokeyr = 0.0317321, 
+          quit_time = -0.0308572)
+
+df$race <- factor(
+  df$race,
+  levels = c(
+    "White",
+    "Black",
+    "Hispanic",
+    "Asian",
+    "American Indian or Alaskan Native",
+    "Native Hawaiian or Pacific Islander"
+  )
+)
+df$smokeday <- (df$smokeday/10)^-1 - 0.4021541613
+df$age <- df$age - 62
+df$education <- df$education - 4
+df$BMI <- df$BMI - 27
+df$smokeyr <- df$smokeyr - 27
+df$quit_time <- df$quit_time - 10
+
+## Get Marker
+X <- model.matrix( ~ age + race + education + BMI + COPD + PHC + FHC +
+                     cigsmok + smokeday + smokeyr + quit_time, data = df )
+logit_M <- X %*% beta
+df$M <- logit_M
+
+
+get_estimators_fast <- function(df, a) {
+  ## Get indices for Y=1 and Y=0
+  idx1 <- df$Y == 1; n1 <- sum(idx1)
+  idx0 <- df$Y == 0; n0 <- sum(idx0)
+  
+  ## Get data for cases and controls
+  df1 <- df[df$Y == 1, , drop = FALSE]
+  df0 <- df[df$Y == 0, , drop = FALSE]
+  
+  
+  
+  ## Normal linear models for M | X, Y, A
+  modM1 <- lm(M ~ A + age + education + BMI + COPD + PHC + FHC +
+                cigsmok + smokeday + smokeyr + quit_time + packyears,
+              data = df1)
+  
+  modM0 <- lm(M ~ A + age + education + BMI + COPD + PHC + FHC +
+                cigsmok + smokeday + smokeyr + quit_time + packyears,
+              data = df0)
+  
+  ## Evaluate everything at intervention arm A = a
+  new_all <- df
+  new_all$A <- a
+  
+  ## Get mu's
+  mu1 <- predict(modM1, newdata = new_all, xlev = list(race = race_lvls))
+  mu0 <- predict(modM0, newdata = new_all, xlev = list(race = race_lvls))
+  
+  ## Get s for Mi-Mj
+  s1     <- summary(modM1)$sigma
+  s0     <- summary(modM0)$sigma
+  s <- sqrt(s1^2 + s0^2)
+  
+  ## Get the case and control mu's 
+  mu1_cases <- mu1[idx1]
+  mu0_controls <- mu0[idx0]
+  
+  ## Get standarized estimator
+  num1 <- 0
+  for (i in 1:sum(idx1)) {
+    num1 <- num1 + sum(pnorm(mu1_cases[i] - mu0_controls, 0, s))
+  }
+  
+  Est1 <- num1/(n1*n0)
+  
+  ## Now for covariate adjusted estimator we need:
+  ## Here we decompose the sum as the sum accross all combinations - sum of equal i = j
+  
+  ## First get outcome model and probabilitys of case and control
+  modY <- glm(Y ~ A + age + education + BMI + COPD + PHC + FHC +
+                cigsmok + smokeday + smokeyr + quit_time + packyears,
+              data = df,
+              family = binomial())
+  
+  PY1 <- predict(modY, newdata = new_all, type = "response",
+                 xlev = list(race = race_lvls))
+  PY0 <- 1 - PY1
+  df$PY1 <- PY1
+  df$PY0 <- PY0
+  
+  ## Numerator m1(Xi,Xj)m2(Xi,Xj)
+  
+  ## sum_ij
+  num11 <- 0
+  for (i in 1:length(mu1)){
+    num11 <- num11 + sum(pnorm(mu1[i] - mu0, 0, s) * PY1[i] * PY0)
+  }
+  
+  ## sum_ii
+  num12 <- sum( pnorm(mu1- mu0, 0, s) * PY1 * PY0)
+  
+  ## Numerator
+  num1 <- num11 - num12 #this numerator will be used in the DR estimation
+  
+  
+  ## Denominator m2(Xi,Xj)   
+  #sumij - sumii
+  den1 <- sum(PY1)*sum(PY0) - sum(PY1*PY0) ## This denominator will be used in the DR estimation
+  
+  ## Get Covariate Adjuste Estimator
+  Est2 <- num1/den1
+  
+  
+  ## Now get the robust estimator:
+  #sum(m1XiXj*m2XiXj + ipw*(I_ind - m1XiXj*m2XiXj), na.rm = T) /
+  #sum(m2XiXj + ipw*(I_10 - m2XiXj),na.rm = T)
+  # We have sum(m1XiXj*m2XiXj ) and sum(m2XiXj) so we focus on the augmentation terms
+  
+  
+  ## Get numerator for IPW part
+  
+  ## Get Propensity Score
+  modA <- glm(A ~ 1, data = df, family = binomial())
+  p1   <- predict(modA, type = "response")
+  PA <- if (a == 1) p1 else 1 - p1
+  
+  ## Get indices for Y=1 and Y=0 on treatment arm A=a
+  idx_case_a  <- which(df$A == a & df$Y == 1)
+  idx_ctrl_a  <- which(df$A == a & df$Y == 0)
+  
+  
+  ## The IPW term is zero when i=j so we focus on sum_ij
+  ## Since we have indicators for Y=1 Y=0 on A=1 we use those indices
+  numIPW <- 0
+  denIPW <- 0
+  for (i in 1:length(idx_case_a)) {
+    numIPW <- numIPW +  sum(1/PA[idx_case_a[i]] * 1/PA[idx_ctrl_a] * as.numeric(df$M[idx_case_a[i]] > df$M[idx_ctrl_a]))
+    denIPW <- denIPW +  sum(1/PA[idx_case_a[i]] * 1/PA[idx_ctrl_a])
+    
+  }
+  
+  ## Now AUgmentation Subctraction term
+  ## Get numerator for substraction term
+  idxA <-  which(df$A == a)
+  
+  numA1 <- 0
+  denA1 <- 0
+  for (i in 1:length(mu1[idxA])){
+    numA1 <- numA1 + sum(pnorm(mu1[idxA][i] - mu0[idxA], 0, s) * PY1[idxA][i] * PY0[idxA] * 1/PA[idxA][i] * 1/PA[idxA])
+    denA1 <- denA1 + sum(PY1[idxA][i] * PY0[idxA] * 1/PA[idxA][i] * 1/PA[idxA])
+  }
+  
+  ## sum_ii
+  numA2 <- sum( pnorm(mu1[idxA]- mu0[idxA], 0, s) * PY1[idxA] * PY0[idxA] * 1/PA[idxA] * 1/PA[idxA])
+  denA2 <- sum(PY1[idxA] * PY0[idxA] * 1/PA[idxA] * 1/PA[idxA])
+  
+  
+  
+  numR <- num1 + numIPW - (numA1 - numA2)
+  denR <- den1 + denIPW - (denA1 - denA2)
+  
+  
+  EstR <- numR/denR
+  
+  
+  empiricalAUC_fast <- function(a, df) {
+    idx <- df$A == a
+    y   <- df$Y[idx]
+    m   <- df$M[idx]
+    n1  <- sum(y == 1L)
+    n0  <- sum(y == 0L)
+    if (n1 == 0L || n0 == 0L) return(NA_real_)
+    r   <- rank(m, ties.method = "average")
+    s1  <- sum(r[y == 1L])
+    (s1 - n1 * (n1 + 1) / 2) / (n1 * n0)
+  }
+  
+  Emp <- empiricalAUC_fast(a, df)
+  
+  return(c(
+    Emp = Emp,
+    Est1      = Est1,
+    Est2      = Est2,
+    EstDR     = EstR
+  ))
+}
+
+
+
+## Bootstrap
+n <- nrow(df)
+B <- 1000
+estimators0 <- matrix(NA_real_, ncol = 4, nrow = B,
+                      dimnames = list(NULL, c("Emp", "Est1", "Est2", "EstDR")))
+set.seed(1)
+for (i in 1:B) {
+  cat("Bootstrap", i, "of", B, "\n")
+  ids <- sample(n, replace = TRUE)
+  df_sample <- df[ids, , drop = FALSE]
+  table(df_sample$race)
+  estimators0[i, ] <- get_estimators_fast(df_sample, 0)
+}
+
+write.csv(estimators0, "/home/mcolonva/Boostrap/fullBoostrap0.csv", row.names = FALSE)
+
---
+++ sim.R
@@ -0,0 +1,580 @@
+## Load Libraries 
+library(ggplot2)
+library(dplyr)
+library(MASS)
+library(tidyr)
+library(kableExtra)
+library(pbapply)    
+library(scales)
+theme_set(theme_minimal())
+
+
+## Data Generating Function
+sim_df <- function(n, muX, sigmaX, alpha0, alphaX, alphaA,
+                   beta0, betaY, betaX, beta_X2, betaA, betaXY, betaAY, sigma0, sigma1){
+  ## Simulate X
+  X <- mvrnorm(n, muX, sigmaX)
+  
+  ## Simulate A
+  A <- rbinom(n, 1, 0.5)
+  
+  ## Simulate Y depending on A and X
+  lpY <- alpha0 + as.numeric(alphaX %*% t(X)) + alphaA *A
+  p <- exp(lpY)/(1+exp(lpY))
+  Y <- rbinom(n,1,p)
+  
+  ## Simulate M such that is normal coniditoned on X,Y,A
+  eps1 <- rnorm(n,0,sigma1)
+  eps0 <- rnorm(n,0,sigma0)
+  
+  M <- beta0 + betaY*Y + as.numeric(betaX %*% t(X)) + beta_X2*X[,1]^2 + 
+    betaA*A + as.numeric(betaXY %*% t(X))* Y + betaAY*Y*A + eps1*Y + eps0*(1-Y)
+  M <- as.numeric(M)
+  df <- data.frame(Y = Y, A = A, M = M, X = X)
+  par <- data.frame(n = n, muX = muX, sigmaX=sigmaX, alpha0=alpha0, 
+                    alphaX=alphaX, alphaA=alphaA, beta0=beta0, betaY=betaY, 
+                    betaX=betaX, beta_X2=beta_X2, betaA=betaA, betaXY=betaXY, betaAY=betaAY, 
+                    sigma0=sigma0, sigma1=sigma1) 
+  return(list(df, par))
+}
+
+
+## Set Parameters
+muX = c(2,-1,1); sigmaX = diag(c(0.3, 0.3, 0.3), nrow = 3, ncol = 3)
+alpha0 = -1 ; alphaX = c(-0.5,-1,1); beta_X2 = 1; alphaA = 1 ;
+beta0 = 1; betaY = 5; betaX = c(2,3,2); betaA = 0.5; betaXY = c(3 ,3.5, 2); betaAY = 4
+sigma0 <- 0.25; sigma1 <- 0.25
+
+
+## Set n: 200, 500, 700, 1000, 2500, 5000
+n <- 1000
+
+## Empirical AUC function 
+empiricalAUC_fast <- function(a, df) {
+  idx <- df$A == a
+  y   <- df$Y[idx]
+  m   <- df$M[idx]
+  n1  <- sum(y == 1L)
+  n0  <- sum(y == 0L)
+  if (n1 == 0L || n0 == 0L) return(NA_real_)
+  r   <- rank(m, ties.method = "average")
+  s1  <- sum(r[y == 1L])
+  (s1 - n1 * (n1 + 1) / 2) / (n1 * n0)
+}
+
+
+## True AUC's
+trueAUC1 <- 0.9496
+trueAUC0 <- 0.8613
+
+
+####################### Correctly Specified Scenario ###########################
+
+get_estimators <- function(a, df){
+  ## 1) Fit a linear regression accross Y=1 and Y=0 separately. 
+  df1 <- df %>% filter(Y==1)
+  df0 <- df %>% filter(Y==0)
+  
+  modM1 <- lm(M ~ A + X.1 + X.2 + X.3 + I(X.1^2), data = df1)
+  summary(modM1)
+  
+  modM0 <- lm(M ~ A + X.1 + X.2 + X.3 + I(X.1^2), data = df0)
+  summary(modM0)
+  
+  ## For A=1
+  new_all <- transform(df, A = a)         
+  mu1 <- predict(modM1, new_all)           
+  mu0 <- predict(modM0, new_all)          
+  
+  df$mu1 <- mu1
+  df$mu0 <- mu0
+  
+  #Get M1XiXj
+  m1XiXj <- pnorm(outer(df$mu1, df$mu0, "-")/sqrt(sigma(modM1)^2 + sigma(modM0)^2))
+  diag(m1XiXj) <- NA_real_
+  
+  
+  #2) Fit outcome model
+  modY <- glm(Y ~ A + X.1 + X.2 + X.3, df, family = "binomial")
+  
+  ## Get M2XiXj
+  df$PY1 <- predict(modY, new_all, type = "response")
+  df$PY0 <- 1-df$PY1
+  m2XiXj <- outer(df$PY1, df$PY0, "*")
+  diag(m2XiXj) <- NA_real_
+  
+  
+  #3) Fit propensity Score Model
+  modA <- glm(A ~ 1, df, family = "binomial")
+  p1 <- predict(modA, df, type = "response")
+  df$PA <- if(a==1) p1 else 1-p1
+  ipw <- outer(as.numeric(df$A==a)/df$PA, as.numeric(df$A==a)/df$PA, "*")
+  diag(ipw) <- NA_real_
+  
+  #4) This is extra for the DR estimator 
+  I_M   <- outer(df$M, df$M, FUN = ">")          # 1{Mi > Mj}
+  I_10  <- outer(df$Y == 1, df$Y == 0, FUN = "&")# 1{Yi=1, Yj=0}
+  I_ind <-  I_M & I_10
+  diag(I_ind) <- NA_real_
+  diag(I_10) <- NA_real_
+  
+  ## Standarized
+  Est1 <-  sum(m1XiXj*I_10, na.rm = T)/sum(I_10, na.rm = T)
+  
+  ## Covariate Adjusted
+  Est2 <- sum(m2XiXj*m1XiXj, na.rm = T)/sum(m2XiXj, na.rm = T)
+  
+  ## DR Estimator
+  EstDR <- sum(m1XiXj*m2XiXj + ipw*(I_ind - m1XiXj*m2XiXj), na.rm = T)/sum(m2XiXj + ipw*(I_10 - m2XiXj),na.rm = T)
+  
+  ## Empirical 
+  Emp <- empiricalAUC_fast(a, df)
+  
+  return(c(Estimator1 = Est1, Estimator2 = Est2, Robust = EstDR, Empirical = Emp))
+}
+
+set.seed(2)
+N_sim <- 5000
+EstimatorsA1 <- matrix(NA, nrow = N_sim, ncol = 4)
+EstimatorsA0 <- matrix(NA, nrow = N_sim, ncol = 4)
+colnames(EstimatorsA1) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+colnames(EstimatorsA0) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+for (i in 1:N_sim) {
+  sim <- sim_df(n, muX, sigmaX, alpha0, alphaX, alphaA,
+                beta0, betaY, betaX, beta_X2, betaA, betaXY, betaAY, sigma0, sigma1)
+  df <- sim[[1]]
+  par <- sim[[2]]
+  EstimatorsA1[i, ] <- get_estimators(1, df)   
+  EstimatorsA0[i, ] <- get_estimators(0, df) 
+  #print(i)
+}
+
+EstimatorsA1 <- as.data.frame(EstimatorsA1) %>% mutate(A = "A=1")
+EstimatorsA0 <- as.data.frame(EstimatorsA0) %>% mutate(A = "A=0")
+Estimators <- rbind(EstimatorsA0, EstimatorsA1)
+Estimators <- Estimators %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%          # A=0 -> 0, etc.
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "estimate"
+  )
+
+
+rel_eff_var1 <- Estimators %>%
+  group_by(A, estimator) %>%
+  summarise(var = var(estimate, na.rm = TRUE), .groups = "drop") %>%
+  group_by(A) %>%
+  mutate(var_unadj = var[estimator == "Empirical"],
+         RE = var_unadj / var) %>%              # >1 ⇒ more efficient than Empirical
+  filter(estimator != "Empirical") %>%
+  dplyr::select(A, estimator, RE)
+rel_eff_var1$scenario <- "both correct"
+
+biasA1 <- EstimatorsA1[,1:4] - trueAUC1
+biasA0 <- EstimatorsA0[,1:4] - trueAUC0
+
+
+biasA1 <- as.data.frame(biasA1) %>% mutate(A = "A=1")
+biasA0 <- as.data.frame(biasA0) %>% mutate(A = "A=0")
+bias <- rbind(biasA0, biasA1) %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%         
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "bias"
+  )
+
+bias1 <- bias %>% 
+  group_by(A, estimator) %>%
+  summarize(Bias = round(mean(bias), 4))
+bias1$scenario <- "both correct"
+
+
+
+################ Correct AUC model but incorrect outcome model #################
+
+get_estimators <- function(a, df){
+  ## 1) Fit a linear regression accross Y=1 and Y=0 separately. 
+  df1 <- df %>% filter(Y==1)
+  df0 <- df %>% filter(Y==0)
+  
+  modM1 <- lm(M ~ A + X.1 + X.2 + X.3 + I(X.1^2), data = df1)
+  summary(modM1)
+  
+  modM0 <- lm(M ~ A + X.1 + X.2 + X.3 + I(X.1^2), data = df0)
+  summary(modM0)
+  
+  ## For A=1
+  new_all <- transform(df, A = a)         
+  mu1 <- predict(modM1, new_all)           
+  mu0 <- predict(modM0, new_all)          
+  
+  df$mu1 <- mu1
+  df$mu0 <- mu0
+  
+  #Get M1XiXj
+  m1XiXj <- pnorm(outer(df$mu1, df$mu0, "-")/sqrt(sigma(modM1)^2 + sigma(modM0)^2))
+  diag(m1XiXj) <- NA_real_
+  
+  
+  #2) Fit outcome model
+  modY <- glm(Y ~ A + X.1 + X.2, df, family = "binomial")
+  
+  ## Get M2XiXj
+  df$PY1 <- predict(modY, new_all, type = "response")
+  df$PY0 <- 1-df$PY1
+  m2XiXj <- outer(df$PY1, df$PY0, "*")
+  diag(m2XiXj) <- NA_real_
+  
+  
+  #3) Fit propensity Score Model
+  modA <- glm(A ~ 1, df, family = "binomial")
+  p1 <- predict(modA, df, type = "response")
+  df$PA <- if(a==1) p1 else 1-p1
+  ipw <- outer(as.numeric(df$A==a)/df$PA, as.numeric(df$A==a)/df$PA, "*")
+  diag(ipw) <- NA_real_
+  
+  #4) This is extra for the DR estimator 
+  I_M   <- outer(df$M, df$M, FUN = ">")          # 1{Mi > Mj}
+  I_10  <- outer(df$Y == 1, df$Y == 0, FUN = "&")# 1{Yi=1, Yj=0}
+  I_ind <-  I_M & I_10
+  diag(I_ind) <- NA_real_
+  diag(I_10) <- NA_real_
+  
+  ## Standarized
+  Est1 <-  sum(m1XiXj*I_10, na.rm = T)/sum(I_10, na.rm = T)
+  
+  ## Estimator 2
+  Est2 <- sum(m2XiXj*m1XiXj, na.rm = T)/sum(m2XiXj, na.rm = T)
+  
+  ## DR Estimator
+  EstDR <- sum(m1XiXj*m2XiXj + ipw*(I_ind - m1XiXj*m2XiXj), na.rm = T)/sum(m2XiXj + ipw*(I_10 - m2XiXj),na.rm = T)
+  
+  ## Empirical 
+  Emp <- empiricalAUC_fast(a, df)
+  
+  return(c(Estimator1 = Est1, Estimator2 = Est2, Robust = EstDR, Empirical = Emp))
+}
+
+set.seed(2)
+EstimatorsA1 <- matrix(NA, nrow = N_sim, ncol = 4)
+EstimatorsA0 <- matrix(NA, nrow = N_sim, ncol = 4)
+colnames(EstimatorsA1) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+colnames(EstimatorsA0) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+for (i in 1:N_sim) {
+  #print(i)
+  sim <- sim_df(n, muX, sigmaX, alpha0, alphaX, alphaA,
+                beta0, betaY, betaX, beta_X2, betaA, betaXY, betaAY, sigma0, sigma1)
+  df <- sim[[1]]
+  par <- sim[[2]]
+  EstimatorsA1[i, ] <- get_estimators(1, df)   
+  EstimatorsA0[i, ] <- get_estimators(0, df) 
+}
+
+EstimatorsA1 <- as.data.frame(EstimatorsA1) %>% mutate(A = "A=1")
+EstimatorsA0 <- as.data.frame(EstimatorsA0) %>% mutate(A = "A=0")
+Estimators <- rbind(EstimatorsA0, EstimatorsA1)
+Estimators <- Estimators %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%          # A=0 -> 0, etc.
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "estimate"
+  )
+
+
+rel_eff_var2 <- Estimators %>%
+  group_by(A, estimator) %>%
+  summarise(var = var(estimate, na.rm = TRUE), .groups = "drop") %>%
+  group_by(A) %>%
+  mutate(var_unadj = var[estimator == "Empirical"],
+         RE = var_unadj / var) %>%              # >1 ⇒ more efficient than Empirical
+  filter(estimator != "Empirical") %>%
+  dplyr::select(A, estimator, RE)
+rel_eff_var2$scenario <- "M1 correct"
+
+biasA1 <- EstimatorsA1[,1:4] - trueAUC1
+biasA0 <- EstimatorsA0[,1:4] - trueAUC0
+
+
+biasA1 <- as.data.frame(biasA1) %>% mutate(A = "A=1")
+biasA0 <- as.data.frame(biasA0) %>% mutate(A = "A=0")
+bias <- rbind(biasA0, biasA1) %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%         
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "bias"
+  )
+
+bias2 <- bias %>% 
+  group_by(A, estimator) %>%
+  summarize(Bias = round(mean(bias), 4))
+bias2$scenario <- "M1 correct"
+
+
+################ Incorrect AUC model but correct outcome model #################
+
+get_estimators <- function(a, df){
+  ## 1) Fit a linear regression accross Y=1 and Y=0 separately. 
+  df1 <- df %>% filter(Y==1)
+  df0 <- df %>% filter(Y==0)
+  
+  modM1 <- lm(M ~ A + X.1 + X.2 + X.3, data = df1)
+  summary(modM1)
+  
+  modM0 <- lm(M ~ A + X.1 + X.2 + X.3, data = df0)
+  summary(modM0)
+  
+  ## For A=1
+  new_all <- transform(df, A = a)         
+  mu1 <- predict(modM1, new_all)           
+  mu0 <- predict(modM0, new_all)          
+  
+  df$mu1 <- mu1
+  df$mu0 <- mu0
+  
+  #Get M1XiXj
+  m1XiXj <- pnorm(outer(df$mu1, df$mu0, "-")/sqrt(sigma(modM1)^2 + sigma(modM0)^2))
+  diag(m1XiXj) <- NA_real_
+  
+  
+  #2) Fit outcome model
+  modY <- glm(Y ~ A + X.1 + X.2 + X.3, df, family = "binomial")
+  
+  ## Get M2XiXj
+  df$PY1 <- predict(modY, new_all, type = "response")
+  df$PY0 <- 1-df$PY1
+  m2XiXj <- outer(df$PY1, df$PY0, "*")
+  diag(m2XiXj) <- NA_real_
+  
+  
+  #3) Fit propensity Score Model
+  modA <- glm(A ~ 1, df, family = "binomial")
+  p1 <- predict(modA, df, type = "response")
+  df$PA <- if(a==1) p1 else 1-p1
+  ipw <- outer(as.numeric(df$A==a)/df$PA, as.numeric(df$A==a)/df$PA, "*")
+  diag(ipw) <- NA_real_
+  
+  #4) This is extra for the DR estimator 
+  I_M   <- outer(df$M, df$M, FUN = ">")          # 1{Mi > Mj}
+  I_10  <- outer(df$Y == 1, df$Y == 0, FUN = "&")# 1{Yi=1, Yj=0}
+  I_ind <-  I_M & I_10
+  diag(I_ind) <- NA_real_
+  diag(I_10) <- NA_real_
+  
+  
+  ## Standarized
+  Est1 <-  sum(m1XiXj*I_10, na.rm = T)/sum(I_10, na.rm = T)
+  
+  ## Estimator 2
+  Est2 <- sum(m2XiXj*m1XiXj, na.rm = T)/sum(m2XiXj, na.rm = T)
+  
+  ## DR Estimator
+  EstDR <- sum(m1XiXj*m2XiXj + ipw*(I_ind - m1XiXj*m2XiXj), na.rm = T)/sum(m2XiXj + ipw*(I_10 - m2XiXj),na.rm = T)
+  
+  ## Empirical 
+  Emp <- empiricalAUC_fast(a, df)
+  
+  return(c(Estimator1 = Est1, Estimator2 = Est2, Robust = EstDR, Empirical = Emp))
+}
+
+set.seed(2)
+EstimatorsA1 <- matrix(NA, nrow = N_sim, ncol = 4)
+EstimatorsA0 <- matrix(NA, nrow = N_sim, ncol = 4)
+colnames(EstimatorsA1) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+colnames(EstimatorsA0) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+for (i in 1:N_sim) {
+  sim <- sim_df(n, muX, sigmaX, alpha0, alphaX, alphaA,
+                beta0, betaY, betaX, beta_X2, betaA, betaXY, betaAY, sigma0, sigma1)
+  df <- sim[[1]]
+  par <- sim[[2]]
+  EstimatorsA1[i, ] <- get_estimators(1, df)   
+  EstimatorsA0[i, ] <- get_estimators(0, df) 
+}
+
+EstimatorsA1 <- as.data.frame(EstimatorsA1) %>% mutate(A = "A=1")
+EstimatorsA0 <- as.data.frame(EstimatorsA0) %>% mutate(A = "A=0")
+Estimators <- rbind(EstimatorsA0, EstimatorsA1)
+Estimators <- Estimators %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%          # A=0 -> 0, etc.
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "estimate"
+  )
+
+
+rel_eff_var3 <- Estimators %>%
+  group_by(A, estimator) %>%
+  summarise(var = var(estimate, na.rm = TRUE), .groups = "drop") %>%
+  group_by(A) %>%
+  mutate(var_unadj = var[estimator == "Empirical"],
+         RE = var_unadj / var) %>%              # >1 ⇒ more efficient than Empirical
+  filter(estimator != "Empirical") %>%
+  dplyr::select(A, estimator, RE)
+rel_eff_var3$scenario <- "M2 correct"
+
+biasA1 <- EstimatorsA1[,1:4] - trueAUC1
+biasA0 <- EstimatorsA0[,1:4] - trueAUC0
+
+
+biasA1 <- as.data.frame(biasA1) %>% mutate(A = "A=1")
+biasA0 <- as.data.frame(biasA0) %>% mutate(A = "A=0")
+bias <- rbind(biasA0, biasA1) %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%         
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "bias"
+  )
+
+bias3 <- bias %>% 
+  group_by(A, estimator) %>%
+  summarize(Bias = round(mean(bias), 4))
+bias3$scenario <- "M2 correct"
+
+
+################# Both AUC model and outcome model icnorrect ###################
+
+get_estimators <- function(a, df){
+  ## 1) Fit a linear regression accross Y=1 and Y=0 separately. 
+  df1 <- df %>% filter(Y==1)
+  df0 <- df %>% filter(Y==0)
+  
+  modM1 <- lm(M ~ A + X.1 + X.2 + X.3, data = df1)
+  summary(modM1)
+  
+  modM0 <- lm(M ~ A + X.1 + X.2 + X.3, data = df0)
+  summary(modM0)
+  
+  ## For A=1
+  new_all <- transform(df, A = a)         
+  mu1 <- predict(modM1, new_all)           
+  mu0 <- predict(modM0, new_all)          
+  
+  df$mu1 <- mu1
+  df$mu0 <- mu0
+  
+  #Get M1XiXj
+  m1XiXj <- pnorm(outer(df$mu1, df$mu0, "-")/sqrt(sigma(modM1)^2 + sigma(modM0)^2))
+  diag(m1XiXj) <- NA_real_
+  
+  
+  #2) Fit outcome model
+  modY <- glm(Y ~ A + X.1 + X.2, df, family = "binomial")
+  
+  ## Get M2XiXj
+  df$PY1 <- predict(modY, new_all, type = "response")
+  df$PY0 <- 1-df$PY1
+  m2XiXj <- outer(df$PY1, df$PY0, "*")
+  diag(m2XiXj) <- NA_real_
+  
+  
+  #3) Fit propensity Score Model
+  modA <- glm(A ~ 1, df, family = "binomial")
+  p1 <- predict(modA, df, type = "response")
+  df$PA <- if(a==1) p1 else 1-p1
+  ipw <- outer(as.numeric(df$A==a)/df$PA, as.numeric(df$A==a)/df$PA, "*")
+  diag(ipw) <- NA_real_
+  
+  #4) This is extra for the DR estimator 
+  I_M   <- outer(df$M, df$M, FUN = ">")          # 1{Mi > Mj}
+  I_10  <- outer(df$Y == 1, df$Y == 0, FUN = "&")# 1{Yi=1, Yj=0}
+  I_ind <-  I_M & I_10
+  diag(I_ind) <- NA_real_
+  diag(I_10) <- NA_real_
+  
+  ## Standarized
+  Est1 <-  sum(m1XiXj*I_10, na.rm = T)/sum(I_10, na.rm = T)
+  
+  ## Estimator 2
+  Est2 <- sum(m2XiXj*m1XiXj, na.rm = T)/sum(m2XiXj, na.rm = T)
+  
+  ## DR Estimator
+  EstDR <- sum(m1XiXj*m2XiXj + ipw*(I_ind - m1XiXj*m2XiXj), na.rm = T)/sum(m2XiXj + ipw*(I_10 - m2XiXj),na.rm = T)
+  
+  ## Empirical 
+  Emp <- empiricalAUC_fast(a, df)
+  
+  return(c(Estimator1 = Est1, Estimator2 = Est2, Robust = EstDR, Empirical = Emp))
+}
+
+
+set.seed(2)
+EstimatorsA1 <- matrix(NA, nrow = N_sim, ncol = 4)
+EstimatorsA0 <- matrix(NA, nrow = N_sim, ncol = 4)
+colnames(EstimatorsA1) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+colnames(EstimatorsA0) <- c("Estimator 1", "Estimator 2", "Robust", "Empirical")
+for (i in 1:N_sim) {
+  sim <- sim_df(n, muX, sigmaX, alpha0, alphaX, alphaA,
+                beta0, betaY, betaX, beta_X2, betaA, betaXY, betaAY, sigma0, sigma1)
+  df <- sim[[1]]
+  par <- sim[[2]]
+  EstimatorsA1[i, ] <- get_estimators(1, df)   
+  EstimatorsA0[i, ] <- get_estimators(0, df) 
+  #print(i)
+}
+
+EstimatorsA1 <- as.data.frame(EstimatorsA1) %>% mutate(A = "A=1")
+EstimatorsA0 <- as.data.frame(EstimatorsA0) %>% mutate(A = "A=0")
+Estimators <- rbind(EstimatorsA0, EstimatorsA1)
+Estimators <- Estimators %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%          # A=0 -> 0, etc.
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "estimate"
+  )
+
+
+rel_eff_var4 <- Estimators %>%
+  group_by(A, estimator) %>%
+  summarise(var = var(estimate, na.rm = TRUE), .groups = "drop") %>%
+  group_by(A) %>%
+  mutate(var_unadj = var[estimator == "Empirical"],
+         RE = var_unadj / var) %>%              # >1 ⇒ more efficient than Empirical
+  filter(estimator != "Empirical") %>%
+  dplyr::select(A, estimator, RE)
+rel_eff_var4$scenario <- "both incorrect"
+
+biasA1 <- EstimatorsA1[,1:4] - trueAUC1
+biasA0 <- EstimatorsA0[,1:4] - trueAUC0
+
+
+biasA1 <- as.data.frame(biasA1) %>% mutate(A = "A=1")
+biasA0 <- as.data.frame(biasA0) %>% mutate(A = "A=0")
+bias <- rbind(biasA0, biasA1) %>%
+  mutate(A = as.integer(sub("^A=", "", A))) %>%         
+  pivot_longer(
+    cols = c( `Estimator 1`,`Estimator 2`, Robust, Empirical),
+    names_to = "estimator",
+    values_to = "bias"
+  )
+
+bias4 <- bias %>% 
+  group_by(A, estimator) %>%
+  summarize(Bias = round(mean(bias), 4))
+bias4$scenario <- "both incorrect"
+
+
+
+## Join RESULTS:
+rel_eff_var1$n <- n
+rel_eff_var2$n <- n
+rel_eff_var3$n <- n
+rel_eff_var4$n <- n
+rel_eff_var_n1 <- rbind(rel_eff_var1, rel_eff_var2, rel_eff_var3, rel_eff_var4)
+
+bias1$n <- n
+bias2$n <- n
+bias3$n <- n
+bias4$n <- n
+bias_n1 <- rbind(bias1, bias2, bias3, bias4)
+
+write.csv(rel_eff_var_n1, "/home/mcolonva/AUC_HS/relative_efficienty4.csv")
+write.csv(bias_n1, "/home/mcolonva/AUC_HS/bias4.csv")
+
+
+
